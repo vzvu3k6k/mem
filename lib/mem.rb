5d1fdd18aca5d9bf2ef13a501792e908f8d4e3f1
vzvu3k6k <vzvu3k6k@gmail.com>
2013-12-20 23:40:10 +0900

    Store memos in @__memo
    This fixes a problem that NameError is raised when you call a
    memoized method whose name is invalid as an instance variable
    (e.g. `oops!` or `huh?`).

--- lib/mem.rb
+++ lib/mem.rb
@@ -3,11 +3,8 @@ require "mem/version"
 module Mem
   def memoize(method_name)
     define_method("#{method_name}_with_memoize") do |*args, &block|
-      if instance_variable_defined?("@#{method_name}")
-        instance_variable_get("@#{method_name}")
-      else
-        instance_variable_set("@#{method_name}", send("#{method_name}_without_memoize", *args, &block))
-      end
+      @__memo ||= {}
+      @__memo[method_name] ||= send("#{method_name}_without_memoize", *args, &block)
     end
     alias_method "#{method_name}_without_memoize", method_name
     alias_method method_name, "#{method_name}_with_memoize"
