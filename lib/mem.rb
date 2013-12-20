38b04a4e9a1bc7019e28bbad4dec75320bb35cb4
vzvu3k6k <vzvu3k6k@gmail.com>
2013-12-21 00:04:14 +0900

    Use closure to store memos
    This fixes a problem that NameError is raised when you call a
    memoized method whose name is invalid as an instance variable
    (e.g. `oops!` or `huh?`).

--- lib/mem.rb
+++ lib/mem.rb
@@ -2,13 +2,18 @@ require "mem/version"
 
 module Mem
   def memoize(method_name)
+    is_called = false
+    memo = nil
+
     define_method("#{method_name}_with_memoize") do |*args, &block|
-      if instance_variable_defined?("@#{method_name}")
-        instance_variable_get("@#{method_name}")
+      if is_called
+        memo
       else
-        instance_variable_set("@#{method_name}", send("#{method_name}_without_memoize", *args, &block))
+        is_called = true
+        memo = send("#{method_name}_without_memoize", *args, &block)
       end
     end
+
     alias_method "#{method_name}_without_memoize", method_name
     alias_method method_name, "#{method_name}_with_memoize"
   end
